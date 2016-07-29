1d1808ba16e6e7a0fdadd1c9b5fc189bbca9b3b2
Valéria Barros <rafael.lee.santos@gmail.com>
2016-07-29 04:56:04 -0300

    Arquivos da primeira versão

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('Champs', [
+  'ngRoute'
+, 'Driver'
+, 'ngAnimate'
+])
+  .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    // 
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/index.html'
+      })
+      .otherwise({redirectTo: '/'});
+  }])
---
+++ assets/js/driver.js
@@ -0,0 +1,85 @@
+function DriverService($http) {
+  const BASE_URL = 'http://ergast.com/api/f1/driverStandings/1.json?limit=11&offset=55';
+  this.list = function() {
+      const request = {
+      url: BASE_URL,
+      method: 'GET'
+    }
+    return $http(request);
+  }
+}
+
+function DriverController(DriverService) { 
+  var vm = this;
+  ((DriverService) => {
+    vm.drivers = [];
+    DriverService
+    .list()
+    .success(function(data){
+      vm.drivers = data.MRData.StandingsTable.StandingsLists;
+    })
+    .error(function(err){
+      console.log('Erro: ', err);
+    });
+  })(DriverService);
+  DriverController.$inject = ['$http'];
+}
+
+function DriverRoundsService($http,$routeParams) { //Service receiving the year and makes the request via Get. Encapsulates in the List and pass to the controller DriverRounds
+  var routeParams = $routeParams;
+  this.driverId = $routeParams.id; //Here the service also receives the parameter to test if the round champion is the champion of the year
+  const BASE_URL = 'http://ergast.com/api/f1/'+ routeParams.season +'/results/1.json';
+  this.list = function() {
+      const request = {
+      url: BASE_URL,
+      method: 'GET'
+    }
+    return $http(request);
+  }
+}
+
+function DriverRoundsController(DriverRoundsService,$routeParams) {
+  var vm = this;
+  vm.driverId = DriverRoundsService.driverId;
+  vm.teste = function (driverId) { //Here I try to create a function that returns me true only if the id Champion coincides with Id being printed and won this Round
+    if (driverId === vm.driverId) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  ((DriverRoundsService) => {
+    vm.rounds = [];
+    DriverRoundsService
+    .list()
+    .success(function(data){ //Through my controller promises to return the data brought via json. It adds to the list, and adds to the array rounds. If something goes wrong, the error is printed on the console.
+      vm.rounds = data.MRData.RaceTable.Races; //Here I take the first part, to the array. And in the view use the rest of the way to go to the field you want to display
+    })
+    .error(function(err){
+      console.log('Erro: ', err);
+    });
+  })(DriverRoundsService);
+  DriverRoundsController.$inject = ['$routeParams','$http'];
+}
+
+angular.module('Driver', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/drivers', {
+        templateUrl: 'views/drivers-list.html',
+        controller: 'DriverController',
+        controllerAs: 'Driver'
+      })
+      .when('/drivers/:season/:id', { //Using the parameters, first to bring the winners of each round, and then to make the comparison by id, if he was the champion of the year and also of each round
+        templateUrl: 'views/rounds-list.html',
+        controller: 'DriverRoundsController',
+        controllerAs: 'DriverRounds'
+      })
+      .otherwise({
+        redirectTo: '/index' //If something goes wrong ...
+      });
+  }])
+  .service('DriverService', DriverService)
+  .controller('DriverController', ['DriverService', DriverController])
+  .service('DriverRoundsService', DriverRoundsService)
+  .controller('DriverRoundsController', ['DriverRoundsService', DriverRoundsController])
